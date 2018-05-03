f7b1c436bc4b0297a1f066c6e844f79546d4f45f
jodusnodus <billiet.thomas@outlook.com>
2018-05-03 18:35:30 +0200

    Allow credential in cors

--- app/application.js
+++ app/application.js
@@ -42,6 +42,7 @@ module.exports.start = function(settings) {
 
   const whitelist = ["http://localhost:3000", "https://thomasbilliet.com"];
   const corsOptions = {
+    credentials: true,
     origin: function(origin, callback) {
       if (whitelist.indexOf(origin) !== -1) {
         callback(null, true);
