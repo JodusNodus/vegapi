1b183c1f72c5b392d9ff51fa324d6813332d10da
jodusnodus <billiet.thomas@outlook.com>
2018-05-03 22:50:51 +0200

    Adjust resolution of images

--- app/service/product-picture.js
+++ app/service/product-picture.js
@@ -1,40 +1,38 @@
-const os = require("os")
-const path = require("path")
-const fs = require("fs")
-const multer  = require("multer")
-const sharp  = require("sharp")
+const os = require("os");
+const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
+const sharp = require("sharp");
 
-const upload = multer({ dest: os.tmpdir() })
+const upload = multer({ dest: os.tmpdir() });
 
-module.exports.upload = upload
+module.exports.upload = upload;
 
-module.exports.process = (file) => sharp(file.path)
-  .trim()
-  .rotate()
-  .normalise()
-  .jpeg({
-    quality: 65
-  })
+module.exports.process = file =>
+  sharp(file.path)
+    .trim()
+    .rotate()
+    .normalise()
+    .jpeg({
+      quality: 65
+    });
 
-module.exports.coverBuffer = (picture) => picture
-  .resize(320, 240)
-  .toBuffer()
-  
-module.exports.thumbBuffer = (picture) => picture
-  .resize(100, 75)
-  .toBuffer()
+module.exports.coverBuffer = picture => picture.resize(400, 300).toBuffer();
 
-const getStream = (ean, baseDir) => new Promise((res, rej) => {
-  const file = path.join(baseDir, ean)
-  fs.exists(file, (exists) => {
-    if (exists) {
-      res(fs.createReadStream(file))
-    } else {
-      rej()
-    }
-  })
-})
+module.exports.thumbBuffer = picture => picture.resize(200, 150).toBuffer();
 
-module.exports.getCoverStream = (ean) => getStream(ean, COVER_DIR)
+const getStream = (ean, baseDir) =>
+  new Promise((res, rej) => {
+    const file = path.join(baseDir, ean);
+    fs.exists(file, exists => {
+      if (exists) {
+        res(fs.createReadStream(file));
+      } else {
+        rej();
+      }
+    });
+  });
 
-module.exports.getThumbStream = (ean) => getStream(ean, THUMB_DIR)
+module.exports.getCoverStream = ean => getStream(ean, COVER_DIR);
+
+module.exports.getThumbStream = ean => getStream(ean, THUMB_DIR);
