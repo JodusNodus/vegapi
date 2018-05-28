b7c2fad305b0c54f384dde1873a69fcb32584abc
jodusnodus <billiet.thomas@outlook.com>
2018-05-28 19:01:24 +0200

    Improve search

--- app/service/products.js
+++ app/service/products.js
@@ -1,75 +1,109 @@
-const _ = require("lodash")
-const args = require("app/args")
-const { knex } = require("app/db")
-const logger = require("app/logger").getLogger("va.service")
+const _ = require("lodash");
+const args = require("app/args");
+const { knex } = require("app/db");
+const logger = require("app/logger").getLogger("va.service");
 
-const nestProductJoins = ({ brandid, brandName, userid, userFirstname, userLastname, hits, rating, ...product }) => ({
+const nestProductJoins = ({
+  brandid,
+  brandName,
+  userid,
+  userFirstname,
+  userLastname,
+  hits,
+  rating,
+  ...product
+}) => ({
   ...product,
   hits: hits || 0,
   rating: rating ? Math.round(rating) : 0,
   brand: { brandid, name: brandName },
-  user: userid ? { userid, firstname: userFirstname, lastname: userLastname } : undefined,
-})
+  user: userid
+    ? { userid, firstname: userFirstname, lastname: userLastname }
+    : undefined
+});
 
-module.exports.paginateAll = async function ({ searchquery, size, page, orderby, labels=[] }) {
+module.exports.paginateAll = async function({
+  searchquery,
+  size,
+  page,
+  orderby,
+  labels = []
+}) {
   let stmt = knex({ p: "products" })
     .join("brands as b", "b.brandid", "p.brandid")
     .leftJoin("productlabels as pl", "pl.ean", "p.ean")
-    .leftJoin("labels as l", "l.labelid", "pl.labelid")
+    .leftJoin("labels as l", "l.labelid", "pl.labelid");
 
   if (labels.length > 0) {
-    stmt.whereIn("l.name", labels)
+    stmt.whereIn("l.name", labels);
   }
 
   if (searchquery) {
-    stmt.whereRaw(`p.name LIKE CONCAT('%',?,'%')`, [ searchquery.toLowerCase() ])
+    stmt.where(function() {
+      this.whereRaw(`MATCH(p.name) AGAINST (? IN NATURAL LANGUAGE MODE)`, [
+        searchquery.toLowerCase()
+      ]).orWhereRaw(`MATCH(b.name) AGAINST (? IN NATURAL LANGUAGE MODE)`, [
+        searchquery.toLowerCase()
+      ]);
+    });
   }
 
-  const totalStmt = stmt.clone().countDistinct("p.ean as count").first()
+  const totalStmt = stmt
+    .clone()
+    .countDistinct("p.ean as count")
+    .first();
 
-  stmt.select("p.ean", "p.name", "p.hits", "p.rating", { brandid: "b.brandid" }, { brandName: "b.name" })
-  stmt.groupBy("p.ean")
+  stmt.select(
+    "p.ean",
+    "p.name",
+    "p.hits",
+    "p.rating",
+    { brandid: "b.brandid" },
+    { brandName: "b.name" }
+  );
+  stmt.groupBy("p.ean");
 
   if (/creationdate|rating|hits/.test(orderby)) {
-      stmt.orderBy(orderby, "desc")
+    stmt.orderBy(orderby, "desc");
   }
 
-  const offset = size * (page - 1)
-  stmt
-    .offset(offset)
-    .limit(size)
+  const offset = size * (page - 1);
+  stmt.offset(offset).limit(size);
 
-  let products = await stmt
-  const { count } = await totalStmt
-  products = products.map(nestProductJoins)
-  return { products, total: count }
-}
+  let products = await stmt;
+  const { count } = await totalStmt;
+  products = products.map(nestProductJoins);
+  return { products, total: count };
+};
 
 module.exports.fetchAllEans = async function(size = 100000, page = 1) {
-  const offset = size * (page - 1)
+  const offset = size * (page - 1);
   return await knex("products")
     .pluck("ean")
     .offset(offset)
-    .limit(size)
-}
+    .limit(size);
+};
 
 module.exports.productExists = async function(ean) {
-  const rows = await knex("products").select("ean").where({ ean })
-  return rows.length > 0
-}
+  const rows = await knex("products")
+    .select("ean")
+    .where({ ean });
+  return rows.length > 0;
+};
 
-const getProductRatingStmt = (ean) => knex("userratings")
+const getProductRatingStmt = ean =>
+  knex("userratings")
     .select(knex.raw("SUM(rating) / COUNT(rating)"))
-    .where({ ean })
+    .where({ ean });
 
-module.exports.fetchProduct = async function (ean, userid) {
+module.exports.fetchProduct = async function(ean, userid) {
   const userHasCorrectedStmt = knex("usercorrections")
     .count()
-    .where({ ean, userid })
-  const ratingStmt = getProductRatingStmt(ean)
+    .where({ ean, userid });
+  const ratingStmt = getProductRatingStmt(ean);
   const userRatingStmt = knex("userratings")
     .select("rating")
-    .where({ ean, userid })
+    .where({ ean, userid });
   const rows = await knex({ p: "products" })
     .select(
       "p.ean",
@@ -82,29 +116,47 @@ module.exports.fetchProduct = async function (ean, userid) {
       { userFirstname: "u.firstname" },
       { userLastname: "u.lastname" },
       "b.brandid",
-      { brandName: "b.name" },
+      { brandName: "b.name" }
     )
     .join("users as u", "u.userid", "p.userid")
     .join("brands as b", "b.brandid", "p.brandid")
-    .where({ ean })
+    .where({ ean });
 
-  if (rows.length == 0) return
+  if (rows.length == 0) return;
 
-  const product = nestProductJoins(rows[0])
-  product.userHasCorrected = !!product.userHasCorrected
-  product.userRating = product.userRating || 0
-  return product
-}
+  const product = nestProductJoins(rows[0]);
+  product.userHasCorrected = !!product.userHasCorrected;
+  product.userRating = product.userRating || 0;
+  return product;
+};
 
-module.exports.insertProduct = async function ({ ean, name, brandid, creationdate, userid }) {
-  await knex("products").insert({ ean, name, brandid, creationdate, userid, hits: 0, rating: 0 })
-}
+module.exports.insertProduct = async function({
+  ean,
+  name,
+  brandid,
+  creationdate,
+  userid
+}) {
+  await knex("products").insert({
+    ean,
+    name,
+    brandid,
+    creationdate,
+    userid,
+    hits: 0,
+    rating: 0
+  });
+};
 
 module.exports.updateProductHits = async function(ean, hits) {
-  await knex("products").where({ ean }).update({ hits })
-}
+  await knex("products")
+    .where({ ean })
+    .update({ hits });
+};
 
 module.exports.updateProductRating = async function(ean) {
-  const ratingStmt = getProductRatingStmt(ean)
-  await knex("products").where({ ean }).update({ rating: ratingStmt })
-}
+  const ratingStmt = getProductRatingStmt(ean);
+  await knex("products")
+    .where({ ean })
+    .update({ rating: ratingStmt });
+};
